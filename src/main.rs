e24337a45ec7aefc97cd2003d67e96117c5d49db
BrokenShell <webmaster@sharpdesigndigital.com>
2021-02-15 12:21:36 -0800

    declarative playtime

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+#[macro_use(c)]
+extern crate cute;
+
 fn main() {
-    println!("Hello, world!");
+    let mut vector = c![x, for x in 1..129, if x % 16 == 0];
+    println!("{:?}", vector);
+    vector.sort();
+    vector.reverse();
+    println!("Sum: {}", vector.iter().take(3).sum::<i16>());
+    println!("{}", i8::MAX);
 }
